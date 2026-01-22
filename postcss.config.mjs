e21d9831a41d15cd628ec7caa1c1a7ae7566572d
brnoliveira110 <brnoliveirasantana@gmail.com>
2026-01-21 22:27:34 -0300

    ajustes na configuração do tailwind

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {
-      content: [
-        './src/components/**/*.{ts,tsx,js,jsx}',
-        './src/app/**/*.{ts,tsx,js,jsx}',
-      ],
-    },
+    "@tailwindcss/postcss": {},
   },
 };
 
